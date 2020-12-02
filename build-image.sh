ae8c811bca9c478991f3089570a6ffd2b74c1fa1
Cong He <43658413+CooHee@users.noreply.github.com>
2020-12-02 10:12:05 +0800

    Add files via upload

---
+++ build-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+workDir="$(dirname "$(readlink -f "$0")")"
+port=8085
+
+image_name="timeclock-img"
+container_name="timeclock"
+
+name_exist=`docker images | grep $image_name`
+if [[ -n "$name_exist" ]]; then
+  docker rm -f $image_name
+fi
+
+docker build -f docker/Dockerfile -t $image_name $workDir
